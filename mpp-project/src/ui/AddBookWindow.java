d571821109e40fe77df622f808c092ed2d6aa160
pain2589 <pain2589@gmail.com>
2017-07-06 16:35:28 -0500

    Remove warning

--- mpp-project/src/ui/AddBookWindow.java
+++ mpp-project/src/ui/AddBookWindow.java
@@ -19,7 +19,6 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
