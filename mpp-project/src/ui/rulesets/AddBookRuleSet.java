5e158ddff33318ba42c0eba724f1c1fcba01ac62
pain2589 <pain2589@gmail.com>
2017-07-06 12:09:19 -0500

    Add new Author feature when add new book

--- mpp-project/src/ui/rulesets/AddBookRuleSet.java
+++ mpp-project/src/ui/rulesets/AddBookRuleSet.java
@@ -14,40 +14,31 @@ final public class AddBookRuleSet implements RuleSet {
 		AddBookWindow addBookWindow = (AddBookWindow) ob;
 		String isbn = addBookWindow.getISBN();
 		String title = addBookWindow.getBookTitle();
-		String maxCheckoutLenght = addBookWindow.getMaxCheckoutLenght();
 		String numberOfCopies = addBookWindow.getNumberOfCopies();
 		Boolean noAuthorSelected = addBookWindow.noAuthorSelected();
-		
-		nonEmptyRule(isbn, title, maxCheckoutLenght, numberOfCopies, noAuthorSelected);
+
+		nonEmptyRule(isbn, title, numberOfCopies, noAuthorSelected);
 		isbnRule(isbn);
-		numbericRule(maxCheckoutLenght, numberOfCopies);
-		maxCheckoutLengthRule(maxCheckoutLenght);
-	}
-	
-	private void maxCheckoutLengthRule(String maxCheckoutLenght) throws RuleException{
-		if (Integer.parseInt(maxCheckoutLenght) != 21 && Integer.parseInt(maxCheckoutLenght) != 7)
-			throw new RuleException("Max checkout lenght value must be 21 or 7.");
+		numbericRule(numberOfCopies);
 	}
 
-	private void numbericRule(String maxCheckoutLenght, String numberOfCopies) throws RuleException{
+	private void numbericRule(String numberOfCopies) throws RuleException {
 		try {
-			Integer.parseInt(maxCheckoutLenght);
 			Integer.parseInt(numberOfCopies);
 		} catch (Exception e) {
-			throw new RuleException("Max checkout lenght and Number of copies must be numberic.");
+			throw new RuleException(" Number of copies must be numberic.");
 		}
 	}
-	
+
 	private void isbnRule(String isbn) throws RuleException {
 		if (!Pattern.matches("(\\d+)(-?)(\\d+)", isbn)) {
 			throw new RuleException("ISBN number only include number and dash sign.");
 		}
 	}
 
-	private void nonEmptyRule(String isbn, String title, String maxCheckoutLen, String numOfCopies,
-			Boolean noAuthorSelected) throws RuleException {
-		if (isbn.trim().isEmpty() || title.trim().isEmpty() || maxCheckoutLen.trim().isEmpty()
-				|| numOfCopies.trim().isEmpty() || noAuthorSelected) {
+	private void nonEmptyRule(String isbn, String title, String numOfCopies, Boolean noAuthorSelected)
+			throws RuleException {
+		if (isbn.trim().isEmpty() || title.trim().isEmpty() || numOfCopies.trim().isEmpty() || noAuthorSelected) {
 			throw new RuleException("All fields must be nonempty.");
 		}
 	}
